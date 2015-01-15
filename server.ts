dfeb6b64d242f6707536f7b7d1052d763e0d3429
mvidailhet <mvidailhet@mncc.fr>
2015-01-15 15:00:04 +0100

    Added specific files

---
+++ server.ts
@@ -0,0 +1,11 @@
+///<reference path="typings/tsd.d.ts" />
+
+var sys = require("sys"),
+my_http = require("http");
+my_http.createServer(function (request, response) {
+    sys.puts("I got kicked");
+    response.writeHeader(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}).listen(8080);
+sys.puts("Server Running on 8080");
